dac0bb0bcd1973f23e46be1c07d0bb23b112a574
Robert Kirkum <robert113289@gmail.com>
2017-12-20 20:52:12 -0600

    settingupAddMenuItems

--- src/CheeseMVC/Controllers/MenuController.cs
+++ src/CheeseMVC/Controllers/MenuController.cs
@@ -3,14 +3,65 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using CheeseMVC.Data;
+using CheeseMVC.Models;
+using CheeseMVC.ViewModels;
+using Microsoft.EntityFrameworkCore;
 
 namespace CheeseMVC.Controllers
 {
     public class MenuController : Controller
     {
+        private readonly CheeseDbContext context;
+        public MenuController(CheeseDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            IList<Menu> allMenus = context.Menus.ToList();
+
+            return View(allMenus);
+        }
+
+        public IActionResult Add()
+        {
+            AddMenuViewModel addMenuViewModel = new AddMenuViewModel();
+            return View(addMenuViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddMenuViewModel addMenuViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                Menu newMenu = new Menu
+                {
+                    Name = addMenuViewModel.Name
+                };
+                context.Menus.Add(newMenu);
+                context.SaveChanges();
+
+                return Redirect("/Menu/ViewMenu/" + newMenu.ID);
+            }
+
+            return View(addMenuViewModel);
+        }
+
+        public IActionResult ViewMenu(int id)
+        {
+            Menu menuRequested = context.Menus.Single(c => c.ID == id);
+            IList<CheeseMenu> items = context.CheeseMenus
+                .Include(item => item.Cheese)
+                .Where(c => c.MenuID == id)
+                .ToList();
+            ViewMenuViewModel viewMenuViewModel = new ViewMenuViewModel
+            {
+                Menu = menuRequested,
+                Items = items
+            };
+            return View(viewMenuViewModel);
         }
     }
 }
